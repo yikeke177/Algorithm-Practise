60e0269b7630d2c8dec24b11e7c4116924110730
jyk-mac <jianyike13@gmail.com>
2026-01-15 10:22:53 +0800

    leetcode 3 20 53 82 88

--- src/com/yike/algorithms/leetcode/order146/LRUCache2.java
+++ src/com/yike/algorithms/leetcode/order146/LRUCache2.java
@@ -1,84 +1,97 @@
 package com.yike.algorithms.leetcode.order146;
 
 import java.util.HashMap;
-import java.util.Map;
 
-/**
- * @author: jyk
- * @description: TODO
- * @date: 2025/12/31 09:40
- * @version: 1.0
- */
 class LRUCache2{
-    static class Node{
-        int key, value;
 
-        Node prev, next;
-        Node(int key, int value){
+    static class Node{
+        private int key, val;
+        private Node pre, next;
+
+        public Node(int key, int val){
             this.key = key;
-            this.value = value;
+            this.val = val;
+            pre = null;
+            next = null;
         }
     }
+
     private int capacity;
-    private Map<Integer, Node> map = new HashMap<>();
-    private Node dummy = new Node(-1, -1);
+    private HashMap<Integer, Node> map;
+    private Node dummy;
 
-    // 构造函数
     LRUCache2(int capacity){
-
         this.capacity = capacity;
-        this.dummy.prev = this.dummy;
-        this.dummy.next = this.dummy;
+        dummy = new Node(-1, -1);
+        dummy.pre = dummy;
+        dummy.next = dummy;
+        map = new HashMap<Integer, Node>();
     }
 
 
     int get(int key){
-        if(! map.containsKey(key)) return -1;
+		/*
+		如果不存在，直接返回-1
+		如果存在
+		删除节点 从头插入节点
+		返回
+		*/
+
         Node node = map.get(key);
-        // 移到最前面 先删除再插入头部
-        this.remove(node);
-        this.putFront(node);
-        return node.value;
+        if(node == null) return -1;
+
+        removeNode(node);
+        addHead(node);
+
+        return node.val;
+
 
     }
 
-    void put(int key, int value){
-        // 存在则更新
-        if(map.containsKey(key)){
-            Node node = map.get(key);
-            node.value = value;
-            this.remove(node);
-            this.putFront(node);
+
+    void put(int key, int val){
+		/*
+		如果存在
+		更新val 删除节点 从头插入
+
+		如果不存在
+		new新节点 从头插入
+		如果map长度超出capacity 删除尾节点
+		*/
+
+        Node node = map.get(key);
+        if(node != null){
+            node.val = val;
+            removeNode(node);
+            addHead(node);
             return;
         }
-        // 不存在则插入头部
-        Node newNode = new Node(key, value);
-        this.map.put(key, newNode);
-        this.putFront(newNode);
 
-        // 如果超出了长度 则删除最后一个节点
-        if(map.size() > this.capacity){
-            Node lastNode = dummy.prev;
-            map.remove(lastNode.key);
-            this.remove(lastNode);
+        Node newNode = new Node(key, val);
+        addHead(newNode);
+        map.put(key, newNode);
 
+        if(map.size() > capacity){
+            map.remove(dummy.pre.key);
+            removeNode(dummy.pre);
         }
 
 
-
     }
 
-    private void remove(Node node){
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-    }
-
-    private void putFront(Node node){
+    void addHead(Node node){
         node.next = dummy.next;
-        node.prev = dummy;
+        node.pre = dummy;
+        dummy.next.pre = node;
         dummy.next = node;
-        node.next.prev = node;
+
     }
+    void removeNode(Node node){
+        node.pre.next = node.next;
+        node.next.pre = node.pre;
+
+    }
+
 
     public static void main(String[] args) {
         LRUCache2 lruCache = new LRUCache2(2);
@@ -98,4 +111,5 @@ class LRUCache2{
 
 
 }
+
 
---
+++ src/com/yike/algorithms/leetcode/order53/Code53.java
@@ -0,0 +1,18 @@
+package com.yike.algorithms.leetcode.order53;
+
+/**
+ * @author: jyk
+ * @description: 53. 最大子数组和
+ * @date: 2026/1/12 10:41
+ * @version: 1.0
+ */
+public class Code53 {
+
+    class Solution {
+        public int maxSubArray(int[] nums) {
+            //
+            return 1;
+        }
+    }
+
+}
